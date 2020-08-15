fdc4704ea5cf5f12e869c473bec736c646f20b76
Luis Ed. Sierra <luis.ed.sierra@gmail.com>
2020-08-15 11:25:50 -0500

    Refactored map impl to manage setting of ID property

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,13 +1,17 @@
 package guru.springframework.sfgpetclinic.services.map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, K> {
+import guru.springframework.sfgpetclinic.model.BaseEntity;
 
-  protected Map<K, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, K extends Long> {
+
+  protected Map<Long, T> map = new HashMap<>();
 
   Set<T> findAll() {
     return new HashSet<>(map.values());
@@ -17,9 +21,15 @@ public abstract class AbstractMapService<T, K> {
     return map.get(id);
   }
 
-  T save(K id, T object) {
-    map.put(id, object);
-    return object;
+  T save(T object) {
+    if (object != null) {
+      if (object.getId() == null) {
+        object.setId(getNextId());
+      }
+      map.put(object.getId(), object);
+      return object;
+    }
+    throw new IllegalArgumentException("Object cannot be null");
   }
 
   void deleteById(K id) {
@@ -31,6 +41,17 @@ public abstract class AbstractMapService<T, K> {
         .removeIf(
             entry -> entry.getValue()
                 .equals(object));
+  }
+
+  private Long getNextId() {
+    Long nextId;
+    try {
+      nextId = Collections.max(map.keySet()) + 1L;
+    }
+    catch (NoSuchElementException e) {
+      nextId = 1L;
+    }
+    return nextId;
   }
 
 }
